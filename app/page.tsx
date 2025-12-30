c9ce26f13f2240fa7da02b3c205e99febb2b6c98
André Félix <afelixfreitas@gmail.com>
2025-12-30 00:30:40 -0300

    feat: added initial explore events button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Home = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn />
     </section>
   );
 };

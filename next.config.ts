93a414a8c2a6af88f39aaae492014a3dbd93bd20
André Félix <afelixfreitas@gmail.com>
2025-12-30 00:10:02 -0300

    chore: initial project config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  cacheComponents: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  logging: {
+    fetches: { fullUrl: true },
+  },
 };
 
 export default nextConfig;
